0c51e6d8a13eeec80d7e1860fe1481c099664d86
OlegHold <cherniyo64@gmail.com>
2024-12-15 19:25:55 +0700

    Add files via upload

---
+++ Lesson4.py
@@ -0,0 +1,16 @@
+
+# module_1_3.py
+
+name="Oleg"
+print(name)
+age=60
+print(age)
+age1=60+1
+print(age1)
+is_student=True
+print(is_student)
+# Итог:
+print("Name: ",name)
+print("Age: ",age)
+print("New Age: ",age1)
+print("is_student: ",is_student)
