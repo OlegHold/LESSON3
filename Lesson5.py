b394949cda6897022a37848ec2327255868d16de
OlegHold <cherniyo64@gmail.com>
2024-12-16 13:57:43 +0700

    Add files via upload

---
+++ Lesson5.py
@@ -0,0 +1,6 @@
+number_completed_homework=12
+number_hours_spent=1.5
+course_name='Python'
+Time_one_task=number_hours_spent/number_completed_homework
+print('Курс:',course_name,', ','всего задач:',number_completed_homework,', затрачено часов:',number_hours_spent,', среднее время выполнения ',Time_one_task,'часа.')
+
