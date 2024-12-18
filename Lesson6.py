bf02ff20fb623eec5b9a2fdc66f917c4ad3db3ae
OlegHold <cherniyo64@gmail.com>
2024-12-18 17:07:04 +0700

    Add files via upload

---
+++ Lesson6.py
@@ -0,0 +1,27 @@
+#Задача 1 "Арифметика":
+
+# "1st program"
+print((9**0.5)*5)
+
+
+# Задача 2 "Логика":
+
+# "2nd program"
+print(9.99>9.98 and 1000!=1000.1)
+
+
+# Задача 3 "Школьная загадка":
+
+# "3rd program"
+print(2*2+2)
+print(2*(2+2))
+print((2*2+2)==(2*(2+2)))
+
+
+# "4th program"
+i='123.456'
+type(float(i))
+print((float(i))*10)
+print(int((float(i))*10))
+print((int((float(i))*10))%1230)
+print(((int((float(i))*10))-1230)/10)
